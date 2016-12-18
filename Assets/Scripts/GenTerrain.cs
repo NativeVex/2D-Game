062a9ac6875e9ca3e41a3669a693e7947c30fbc0
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2016-12-18 17:52:47 -0500

    I exist only to please @vk... Ha.. as if. Enjoy rigging your own character!

---
+++ Assets/Scripts/GenTerrain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public struct GenTerrain {
+	private int dim;
+	private static Sprite[] prefabs;
+	Sprite[][] Sto {
+		get {
+			return new Sprite[dim][dim];
+		}
+	}
+	// Use this for initialization
+	GenTerrain (int dim) {
+		this.dim = dim;
+		//gen terrain here
+	
+	}
+
+	void addTo(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
