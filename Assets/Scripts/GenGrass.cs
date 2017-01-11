8b25a99024cbf9d90a2e3dac098e1dec8bdab398
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2017-01-11 13:06:40 -0500

    restore deleted branch progress

---
+++ Assets/Scripts/GenGrass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System;
+
+public class GenGrass : MonoBehaviour {
+	public GameObject[] prefabs;
+	public int dim;
+	private GameObject[,] Sto;
+	public float scale;
+	public int grassSpeedLimit;
+
+	// Use this for initialization
+	void Start () {
+		Sto = new GameObject[dim,dim];
+		for (int x = 0; x < dim; x++) {
+			for (int z = 0; z < dim; z++) {
+				Sto [x, z] = (GameObject)Instantiate (prefabs [0], new Vector3 (x * scale*Random.value, 0, z * scale*Random.value), Quaternion.identity);
+				Sto [x, z].GetComponent<Animator> ().SetFloat ("speedMultiplier", Random.value*grassSpeedLimit); 
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GrassAnimSpeed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassAnimSpeed : MonoBehaviour {
+	//public Animation anim;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Animation temp = (Animation)this.GetComponent ("Animation");
+		temp ["grass"].speed = Random.value * this.speed;
+
+		//<Animation>().Play("grass").speed = this.speed*Random.value;
+		//anim ["grass"].speed = this.speed*Random.value;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
