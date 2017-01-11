e00ed618f9bcb9573a536a2c1a7b5225e0cead35
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2017-01-11 18:37:05 -0500

    No idea why this just showed up now

--- Assets/Scripts/GenGrass.cs
+++ Assets/Scripts/GenGrass.cs
@@ -14,7 +14,8 @@ public class GenGrass : MonoBehaviour {
 		Sto = new GameObject[dim,dim];
 		for (int x = 0; x < dim; x++) {
 			for (int z = 0; z < dim; z++) {
-				Sto [x, z] = (GameObject)Instantiate (prefabs [0], new Vector3 (x * scale*Random.value, 0, z * scale*Random.value), Quaternion.identity);
+				int[] locations = {x-(dim/2),z-(dim/2)};
+				Sto [x, z] = (GameObject)Instantiate (prefabs [0], new Vector3 (locations[0] * scale*Random.value, 0, locations[1] * scale*Random.value), Quaternion.identity);
 				Sto [x, z].GetComponent<Animator> ().SetFloat ("speedMultiplier", Random.value*grassSpeedLimit); 
 			}
 		}
