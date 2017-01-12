c9a22cf308091b9a032265b22b92d47bd94814f2
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2017-01-11 20:41:32 -0500

    Just adding a few assets that I figure we're gonna need

---
+++ Assets/RotateWithCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWithCamera : MonoBehaviour {
+	Camera reference;
+	void Start(){
+		reference = (Camera)this.gameObject.GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+}
