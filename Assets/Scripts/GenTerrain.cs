e48472463059599aa7fd432f040d14e1b028b121
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2017-01-16 00:53:56 -0500

    Added @vk's assets through using packages. Git merge proved to be much harder than I anticipated, and this was much easier.

---
+++ Assets/Scripts/GenTerrain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GenTerrain: MonoBehaviour {
+	public GameObject[] prefabs;
+	public GameObject[,] Sto;
+	public int dim;
+	public int scale;
+	// Use this for initialization
+	void Start() {
+		//gen default terrain here
+		for (int x = 0; x < dim; x++) {
+			for (int y = 0; y < dim; y++) {
+				Sto [x, y] = (GameObject)Instantiate(prefabs [0]
+					,new Vector3(x*scale,y*scale,0)
+					,Quaternion.identity);
+			}
+		}
+	}
+		
+
+	void addTo(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
