ef26a3ee5a22a18e3531b8e7847ca580113f9de9
linje2 <linje2@bxscience.edu>
2017-01-16 22:33:47 -0500

    'Village House stuff?'

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+	
+	void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Begin()
+    {
+        SceneManager.LoadScene("level 1");
+    }
+}
