08512c0d9beeda26269ffc708102374790ec4618
linje2 <linje2@bxscience.edu>
2017-01-19 09:58:37 -0500

    'Nothing'

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -5,7 +5,13 @@ public class AttackScript : MonoBehaviour {
     public string direction;
     float speed = 1f;
     Animator anim;
-	void Start () {
+
+    public const int STATE_FRONT_ATTACK = 2;
+    public const int STATE_BACK_ATTACK = 6;
+    public const int STATE_LEFT_ATTACK = 10;
+    public const int STATE_RIGHT_ATTACK = 14;
+
+    void Start () {
         anim = this.gameObject.GetComponent<Animator>();
 	}
 	
@@ -16,21 +22,44 @@ public class AttackScript : MonoBehaviour {
         }
         else
         {
-            if (direction.Equals("up"))
+            if (gameObject.tag.Equals("Player"))
             {
-                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                if (direction.Equals("up"))
+                {
+                    gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("down"))
+                {
+                    gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("left"))
+                {
+                    gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("right"))
+                {
+                    gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
             }
-            else if (direction.Equals("down"))
+            else if (gameObject.tag.Equals("Enemy"))
             {
-                gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
-            }
-            else if (direction.Equals("left"))
-            {
-                gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
-            }
-            else if (direction.Equals("right"))
-            {
-                gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+                if (direction.Equals("up"))
+                {
+                    
+                    gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("down"))
+                {
+                    gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("left"))
+                {
+                    gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                else if (direction.Equals("right"))
+                {
+                    gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
             }
         }
     }
