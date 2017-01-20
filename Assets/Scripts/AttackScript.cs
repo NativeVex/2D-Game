df12e0adbbd2e84b5cf7477c3c917dbd025c184d
Artur Janik <Artur@Arturs-MacBook-Pro.local>
2017-01-20 00:01:05 -0500

    this might help a bit

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -3,53 +3,32 @@ using System.Collections;
 
 public class AttackScript : MonoBehaviour {
 	public Vector3 direction;
-    public float speed = 1f;
+	public float speed = 1f;
 	public string[] tags;
 
-    Animator anim;
+	Animator anim;
+	GameObject Parent;
 
-    public bool ATTACK = false;
-    public const int DIR_FRONT = 0, DIR_BACK = 1, DIR_LEFT = 2, DIR_RIGHT = 3;
-    public int direction;
-
-    void Start () {
+	void Start () {
 		Parent = this.gameObject.transform.parent.gameObject;
 		anim = Parent.gameObject.GetComponent<Animator>();
 		Parent.transform.position = direction;
 	}
-	
-	void Update () {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName(""))//change to foreach tags
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            anim.SetInteger("Dir", direction);
-            if (direction == DIR_FRONT)
-            {
-                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
-            else if (direction == DIR_BACK)
-            {
-                gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
-            }
-            else if (direction == DIR_LEFT)
-            {
-                gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
-            }
-            else if (direction == DIR_RIGHT)
-            {
-                gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
-            }
-        }
-    }
 
+	void Update () {
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Destroy"))
+		{
+			Destroy(gameObject.transform.parent.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collider col)
+	{
 		foreach(string temp in tags){
 			if (col.gameObject.tag.Equals(temp))
-	        {
-	            Destroy(gameObject);
-	        }
+			{
+				Destroy(gameObject);
+			}
 		}
-    }
+	}
 }
