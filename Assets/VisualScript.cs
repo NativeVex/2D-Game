228fd4d10bd619a065a893f295fb3079def74ab7
linje2 <linje2@bxscience.edu>
2017-02-09 18:39:55 -0500

    Attack Changes

--- Assets/VisualScript.cs
+++ Assets/VisualScript.cs
@@ -5,17 +5,47 @@ public class VisualScript : MonoBehaviour {
 	public string[] tags;
 	public float dmg;
 	public GameObject AttackScript;
-    public string AttackObj;
-   // public GameObject Player;
+   // public string AttackObj;
+    public int direction;
+    public float runSpeed;
 
-	// Use this for initialization
-	void Start () {
-		AttackScript = Instantiate (AttackScript);
+    // Use this for initialization
+    void Start()
+    {
+        AttackScript = Instantiate(AttackScript);
         //  Player = Instantiate(Player);
-        AttackScript.GetComponent<AttackScript>().AttackObject = AttackObj;
-		AttackScript.GetComponent<AttackScript> ().dmg = dmg;
-		AttackScript.GetComponent<AttackScript> ().tags = tags;
-		AttackScript.GetComponent<AttackScript> ().Parent = gameObject;
-	}
+      //  AttackScript.GetComponent<AttackScript>().direction = direction;
+        AttackScript.GetComponent<AttackScript>().dmg = dmg;
+        AttackScript.GetComponent<AttackScript>().tags = tags;
+        AttackScript.GetComponent<AttackScript>().Parent = gameObject;
+    }
+
+    void Update()
+    {
+        Move();
+    
+    }
+    void Move()
+    {
+        switch (direction)
+        {
+            case 0:
+                
+                gameObject.transform.Translate(Vector3.back * runSpeed * Time.deltaTime);
+                break;
+            case 1:
+               
+                gameObject.transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+                break;
+            case 2:
+               
+                gameObject.transform.Translate(Vector3.right * runSpeed * Time.deltaTime);
+                break;
+            case 3:
+             
+                gameObject.transform.Translate(Vector3.left * runSpeed * Time.deltaTime);
+                break;
+        }
+    }
 
 }
